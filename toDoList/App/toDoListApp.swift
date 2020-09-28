5e89bd0e6cdeda08d9eb6cd4f1f5d79d79ea652c
Dobuzi <kimjw7981@gmail.com>
2020-09-29 06:56:41 +0900

    Link with Firebase and Sign in with anonymous

--- toDoList/App/toDoListApp.swift
+++ toDoList/App/toDoListApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Auth.auth().signInAnonymously()
+        return true
+    }
+}
 
 @main
 struct toDoListApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             TaskListView()

951f89ba0b0d4dee8f03ef20c8064038220a2773
Dobuzi <kimjw7981@gmail.com>
2020-09-28 11:30:20 +0900

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by 김종원 on 2020/09/28.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
